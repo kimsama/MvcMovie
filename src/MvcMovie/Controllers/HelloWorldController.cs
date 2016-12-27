4ba8c4185414484f125c3b113b6cd579d30c2c96
kimsama <hwpeterkim@gmail.com>
2016-12-27 18:23:10 +0900

    Add project files.

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        //
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        //
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return "This is the Welcome action method...";
+            //return System.Text.Encodings.Web.HtmlEncoder.Default.Encode(
+            //       "Hello " + name + ", NumTimes is: " + numTimes);
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
